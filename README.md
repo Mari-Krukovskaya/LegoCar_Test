0690070fb4b8896b97d7df71ffd150d6a6aa0405
Maria Kruk <mari.krukovskaya.97@mail.ru>
2024-04-11 13:01:25 +0300

    vercel projects

---
+++ README.md
@@ -0,0 +1,5 @@
+## Description:
+
+Tests LegoCar
+
+[Website](https://lego-car-test.vercel.app/ "Test_auto")
--- js/index.js
+++
@@ -1,2 +0,0 @@
-const menu = document.querySelector('menu_list');
-const burgerBtn = document.querySelector('mobile_burger');
